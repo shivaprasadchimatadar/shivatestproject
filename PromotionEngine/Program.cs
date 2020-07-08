ea56ad758edc65c8a37914d979847acc27d642bd
Shivaprasad Chimatadar <shivaprasad.chimatadar@siemens-healthineers.com>
2020-07-08 09:07:07 +0530

    Implementating promotion type 2

--- PromotionEngine/Program.cs
+++ PromotionEngine/Program.cs
@@ -6,19 +6,26 @@ namespace PromotionEngine
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World!");
             BuildPromotionType1Map();
             
         }
 
 
         Dictionary<char, Promotiontype1> pt1 = new Dictionary<char, Promotiontype1>();
-    
+        Dictionary<string, int> pt2 = new Dictionary<string, int>();
         
         public static void BuildPromotionType1Dict(char skuId, Promotiontype1 promotiontype1){
             pt1.Add(skuId, promotiontype1);
 
         }
+        public static void BuildPromotionType2Dict(char skuId1, char skuId2, int price){
+            string temp1 = skuId1 + skuId2;
+            string temp2 = skuId2 + skuId1;
 
+            pt2.Add(temp1, price);
+            pt2.Add(temp2, price);
+
+        }
     }
 }
