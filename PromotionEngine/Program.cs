81254dc18658776f4abeb587a0edce70383c5421
Shivaprasad Chimatadar <shivaprasad.chimatadar@siemens-healthineers.com>
2020-07-08 08:33:07 +0530

    Creating main class

---
+++ PromotionEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromotionEngine
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
